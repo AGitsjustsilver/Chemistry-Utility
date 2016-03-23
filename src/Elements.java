65ba6e138284a5da59d47d31ad36ecfb6d72e349
Alessandro Guaresti <alessguar@gmail.com>
2016-03-23 16:51:12 -0700

    initial stuff

---
+++ src/Elements.java
@@ -0,0 +1,46 @@
+
+public class Elements{
+	
+	private String[] elementName = {"Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickle", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismusth", "Polomium", "Astatine", "Radon", "Francium", "Randium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americum", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendeledvium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstandtium", "Roentgenium", "Copernicium", "Ununtrium", "Fleovium", "Ununpentium", "Livermorium", "Ununseptium", "Ununoctium"};
+	private String[] elementSymbol = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Uut", "Fl", "Uup", "Lv", "Uus", "Uuo"};
+	private int[] atomicNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118};
+	private double[] atomicMass = {1.01, 4.00, 6.94, 9.01, 10.81, 12.01, 14.01, 16.00, 19.00, 20.18, 22.99, 24.31, 26.98, 28.09, 30.97, 32.07, 35.45, 39.95, 39.10, 40.08, 44.96, 47.87, 50.94, 52.00, 54.94, 55.85, 58.93, 58.69, 63.55, 65.39, 69.72, 72.61, 74.92, 78.96, 79.90, 83.80, 85.47, 87.62, 88.91, 91.22, 92.91, 95.94, 98, 101.07, 102.91, 106.42, 107.87, 112.41, 114.82, 118.71, 121.76, 127.60, 126.90, 131.29, 132.91, 137.33, 138.91, 140.12, 140.91, 144.24, 145, 150.36, 151.96, 157.25, 158.93, 162.50, 164.93, 167.26, 168.93, 173.04, 174.97, 179.49, 180.95, 183.84, 186.21, 190.23, 192.22, 195.08, 196.97, 200.59, 204.38, 207.2, 208.98, 209, 210, 222, 223, 226, 227, 232.04, 231.04, 238.03, 237, 244, 243, 247, 247, 251, 252, 257, 258, 259, 262, 261, 262, 266, 264, 269, 268, 281, 280, 285, 284, 289, 288, 293, 294, 294};
+	private int find;
+	
+	public Elements(int num){
+		this.find = num-1;
+	}
+	
+	public Elements(String name){
+		for(int index = 0; index < atomicNumber.length; index++){
+			if(elementName[index].equalsIgnoreCase(name)){
+				this.find = index - 1;
+			}
+		}
+	}
+	
+	public String getElementName(int find){
+		return elementName[find]; 
+	}
+	
+	public String getElementSymbol(int find){
+		return elementSymbol[find];
+	}
+	
+	public int getAtomicNumber(int find){
+		return atomicNumber[find];
+	}
+	
+	public double getAtomicMass(int find){
+		return atomicMass[find];
+	}
+	
+	public String toString(){
+		return "For the Element " + getElementName(find) + ".\n"
+				+ "The Symbol is: " + getElementSymbol(find) + ".\n"
+				+ "The Atomic Number is: " + getAtomicNumber(find) + ".\n"
+				+ "The Atomic Mass is: " + getAtomicMass(find) + ".";
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class Main {
+	
+	public static void main(String[] arg){
+		boolean con = true;
+		Scanner in = new Scanner(System.in);
+		System.out.println("What element do you want to know about?");	
+		while (con == true){
+			int num = in.nextInt();
+			if(num == 0 || num == 119){
+				if(num == 0){
+					System.out.println("There is no Element 0. Choose another one.");
+				}else if(num == 119){
+					System.out.println("There is no Element 119. Choose another one.");
+				}
+			}else{
+				Elements el = new Elements(num);
+				System.out.println(el.toString());	
+			}	
+		}
+		in.close();
+	}
+	
+}
+/**
+ * Idea list for program
+ * 
+ * 1.Elements reference
+ * 2.molar-mass finder
+ * 3.moles calculator
+ * 
+ **/
