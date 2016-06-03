9fcd7f138d8a18d65997d6f88e425ee6b79ff7fe
Alessandro Guaresti <alessguar@gmail.com>
2016-06-03 12:25:44 -0700

    Changing the way I put scenes on the display
    Made the menu scene as a new method
    probably going to do that for future menus and stuf
    
    Note to future self :
    make a menu bar on top and the scene methods could be used for that

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -7,17 +8,27 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Display extends Application{
-	
+public class Display extends Application {
 	private Elements in;
+	private Main w;
 	
 	public static void main(String[] args){
 		launch(args);
 	}
 	
 	public void start(Stage primaryStage){
+		firstMenu(primaryStage);
 		
+		
+				
+		primaryStage.setTitle("Chemistry-Utility");
+
+		primaryStage.show();
+	}
+	
+	public static void firstMenu(Stage p){
 		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		
@@ -26,23 +37,30 @@ public class Display extends Application{
 		title.setFont(Font.font(25));
 		grid.add(title, 0, 0);
 		
-		Text rules = new Text();
-		rules.setText("If you want to Input an element's name and get information type: A");
-		grid.add(rules, 0, 1);
+		addText(grid, "If you want to Input an element's name and get information type: A", 0, 1);
+
+		addText(grid, "If you know the element's Atomic Number type: B", 0, 2);
 		
+		addText(grid, "If you know the element's Symbol type: C", 0, 3);
+		
+		addText(grid, "If you want to know the molar mass of any molecule type: D", 0, 4);
 		
 		TextField response = new TextField();
-		grid.add(response, 0, 2);
+		grid.add(response, 0, 5);
 		
 		Button submit = new Button("Submit");
-		grid.add(submit, 2, 3);
+		grid.add(submit, 2, 5);
 		
-		Scene firstChoice = new Scene(grid, 600, 500);
+		Scene firstChoice = new Scene(grid, 600, 400);
 		
-				
-		primaryStage.setTitle("Chemistry-Utility");
-		primaryStage.setScene(firstChoice);
-		primaryStage.show();
+		p.setScene(firstChoice);
+	}
+	
+	public static void addText(GridPane grid, String text, int col, int row){
+		Text rules = new Text();
+		rules.setText(text);
+		grid.add(rules, col, row);
+		
 	}
 	
 }
